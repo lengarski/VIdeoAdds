f277672244444c60e914ebb10e7e04c7fefec1a4
lengarski <lengarski@gmail.com>
2020-03-19 09:15:52 +0200

    Fix position of webView

--- app/src/main/java/eu/lengarski/videoadds/MainActivity.java
+++ app/src/main/java/eu/lengarski/videoadds/MainActivity.java
@@ -1,8 +1,13 @@
 package eu.lengarski.videoadds;
 
+import android.content.Context;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.Display;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowManager;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ImageView;
@@ -17,11 +22,11 @@ public class MainActivity
         extends AppCompatActivity
 {
 
-    private WebView wv;
+
     private ConstraintLayout layout;
 
     private ImageView iv;
-
+    private int PIC_WIDTH = 1024;
     @RequiresApi (api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -44,8 +49,8 @@ public class MainActivity
         webView.getSettings().setJavaScriptEnabled(true);
         webView.getSettings().setAppCacheEnabled(true);
 
-        webView.setScaleX(.7f);
-        webView.setScaleY(.7f);
+//        webView.setScaleX(.7f);
+//        webView.setScaleY(.7f);
 //webView.getSettings().set
 
         webView.loadUrl("https://recognified.com/Showroom/adops/assets/preview/creatives/2020/03/12/4711_RemixCologne_In-Image_Wrap_a2/v1/index.html?t=0");
@@ -53,29 +58,41 @@ public class MainActivity
         webView.setBackgroundColor(Color.TRANSPARENT);
 
 
+        webView.setInitialScale(1);
+        webView.getSettings().setJavaScriptEnabled(true);
         webView.getSettings().setLoadWithOverviewMode(true);
         webView.getSettings().setUseWideViewPort(true);
 
-                Toast.makeText(getApplicationContext()," "+ webView.getWidth() + " width ", Toast.LENGTH_SHORT).show();
+//        webView.setPadding(0, 0, 0, 0);
+//        webView.setInitialScale(getScale());
 
-//        webView.setOnTouchListener(new View.OnTouchListener() {
-//            @Override
-//            public boolean onTouch(View v, MotionEvent event) {
-//                Toast.makeText(getApplicationContext(),"Asdf", Toast.LENGTH_SHORT).show();
-//                return true;
-//            }
-//        });
+        Toast.makeText(getApplicationContext(), " " + webView.getWidth() + " width ", Toast.LENGTH_SHORT).show();
+
+        webView.setOnTouchListener(new View.OnTouchListener()
+        {
+            @Override
+            public boolean onTouch(View v, MotionEvent event)
+            {
+                Toast.makeText(getApplicationContext(), "Asdf", Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+
 
         ConstraintSet set = new ConstraintSet();
         set.clone(layout);
+
+        int webViewWidth = webView.getWidth();
+        int webViewHeight = webView.getHeight();
+
         set.connect(webView.getId(), ConstraintSet.TOP, iv.getId(), ConstraintSet.TOP, 0);
         set.connect(webView.getId(), ConstraintSet.BOTTOM, iv.getId(), ConstraintSet.BOTTOM, 0);
         set.connect(webView.getId(), ConstraintSet.LEFT, iv.getId(), ConstraintSet.LEFT, 0);
         set.connect(webView.getId(), ConstraintSet.RIGHT, iv.getId(), ConstraintSet.RIGHT, 0);
 
 
-        set.setScaleX(webView.getId(),.4f);
-        set.setScaleY(webView.getId(),.4f);
+//        set.setScaleX(webView.getId(),.4f);
+//        set.setScaleY(webView.getId(),.4f);
 //        ConstraintLayout.LayoutParams lp = new ConstraintLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
 //        lp.leftToLeft = iv.getId();
 //        lp.topToTop = iv.getId();
@@ -84,7 +101,8 @@ public class MainActivity
 //
 //        webView.setLayoutParams(lp);
 
-        layout.addView(webView,640,360);
+
+        layout.addView(webView, iv.getWidth(), iv.getHeight());
 
         set.applyTo(layout);
 
@@ -93,5 +111,17 @@ public class MainActivity
 //        layout.addView();
 
 
+    }
+
+
+    
+
+    private int getScale()
+    {
+        Display display = ((WindowManager) getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
+        int width = display.getWidth();
+        Double val = new Double(width) / new Double(PIC_WIDTH);
+        val = val * 100d;
+        return val.intValue();
     }
 }
